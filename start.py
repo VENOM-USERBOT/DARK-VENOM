90829276e92eb5810f537a60e5118f52fce9be11
Inuka Asith <inukaasith7@gmail.com>
2021-06-10 19:09:26 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from os import system 
-system ("git clone https://GitHub.com/VENOM-USERBOT/DARK-VENOM && cd dark-venom && python3 -m userbot")
+system ("git clone https://GitHub.com/VENOM-USERBOT/DARK-VENOM && cd DARK-VENOM && python3 -m userbot")
 # If Pro
 print ("Done your Userbot Deployed !")
