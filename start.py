18b5b0facfa49f048047ee1690fe4f34bba18d35
SHIVANSH <75574324+errorshivansh@users.noreply.github.com>
2021-08-03 08:10:54 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,17 @@
-from os import system 
-system ("git clone https://GitHub.com/VENOM-USERBOT/DARK-VENOM && cd DARK-VENOM && python3 -m userbot")
-# If Pro
-print ("Done your Userbot Deployed !")
+import os 
+import subprocess
+from logging import DEBUG, INFO, basicConfig, getLogger, warning
+basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
+LOGS = getLogger("Helper")
+os.system("git clone https://github.com/VENOM-USERBOT/DARK-VENOM venom")
+os.chdir("venom")
+process = subprocess.Popen(
+        ["python3", "-m", "userbot"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,)
+out, er = process.communicate()
+if er:
+    LOGS.warning(er.decode())
+print("::::::::::::::")
+if out:
+    LOGS.info(out.decode())
